e539bdbff7f3ba299bf6decde376720a720ce166
Lupe <lupemaydana@gmail.com>
2025-04-25 19:08:13 -0400

    feat: add sort by rating

--- src/data.js
+++ src/data.js
@@ -6,14 +6,28 @@ export const filterData = (data, filterBy, value) => {
 export const sortData = (data, sortBy, sortOrder) => {
   const newData = [...data]; // Create a shallow copy to avoid mutating the original array
   return newData.sort((a, b) => {
-    const valueA = a[sortBy];
-    const valueB = b[sortBy];
-    if (sortOrder === "asc") {
-      return valueA - valueB;
-    } else if (sortOrder === "desc") {
-      return valueB - valueA;
+    let valueA = a[sortBy];
+    let valueB = b[sortBy];
+    switch (sortBy) {
+      case 'year_of_release':
+        if (sortOrder === "asc") {
+          return valueA - valueB;
+        } else if (sortOrder === "desc") {
+          return valueB - valueA;
+        }
+        break;
+      case 'rating':
+        valueA = parseFloat(valueA.split("/")[0]);
+        valueB = parseFloat(valueB.split("/")[0]);
+        if (sortOrder === "asc") {
+          return valueA - valueB;
+        } else if (sortOrder === "desc") {
+          return valueB - valueA;
+        }
+        break;
+      default:
+        return 0; // Return 0 if sortBy or sortOrder is invalid, preserving original order
     }
-    return 0; // Return 0 if sortBy or sortOrder is invalid, preserving original order
   });
 };
 
